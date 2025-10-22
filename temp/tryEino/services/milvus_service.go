36928900b44cc236ebda601a626b2bc864304348
张锐杰 <zhangruijie@induschain.cn>
2025-10-22 17:07:05 +0800

    feat: 添加milvus集合定义及插入搜索方法

--- temp/tryEino/services/milvus_service.go
+++ temp/tryEino/services/milvus_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+
 	"github.com/Altergom/tryEino/config"
 	"github.com/milvus-io/milvus-sdk-go/v2/client"
 	"github.com/milvus-io/milvus-sdk-go/v2/entity"
@@ -11,6 +12,18 @@ import (
 type MilvusService struct {
 	client client.Client
 	cfg    *config.Config
+}
+
+type Document struct {
+	ID      string
+	Content string
+	Vector  []float32
+}
+
+type SearchResult struct {
+	ID      string
+	Content string
+	Score   float32
 }
 
 func NewMilvusService(cfg *config.Config) (*MilvusService, error) {
@@ -81,4 +94,69 @@ func (ms *MilvusService) initCollection() error {
 
 func (ms *MilvusService) Close() error {
 	return ms.client.Close()
+}
+
+func (ms *MilvusService) InsertDocument(doc []Document) error {
+	ctx := context.Background()
+	ids := make([]string, len(doc))
+	contents := make([]string, len(doc))
+	vector := make([][]float32, len(doc))
+
+	for i, d := range doc {
+		ids[i] = d.ID
+		contents[i] = d.Content
+		vector[i] = d.Vector
+	}
+
+	_, err := ms.client.Insert(ctx, ms.cfg.CollectionName, "",
+		entity.NewColumnVarChar("id", ids),
+		entity.NewColumnVarChar("content", contents),
+		entity.NewColumnFloatVector("vector", ms.cfg.VectorDim, vector),
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ms *MilvusService) Search(queryVector []float32, topK int) ([]SearchResult, error) {
+	ctx := context.Background()
+	// 创建搜索参数
+	searchParam, _ := entity.NewIndexIvfFlatSearchParam(10)
+	// 开始搜索
+	vectors := []entity.Vector{entity.FloatVector(queryVector)}
+	searchRes, err := ms.client.Search(ctx, ms.cfg.CollectionName, []string{}, "",
+		[]string{"id", "content"}, vectors, "vector", entity.L2, topK, searchParam)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// 处理搜索结果
+	var results []SearchResult
+	for _, searchResult := range searchRes {
+		// 获取ID列
+		ids := searchResult.IDs
+		// 获取字段数据
+		fields := searchResult.Fields
+		// 获取分数
+		scores := searchResult.Scores
+
+		// 遍历每个结果
+		for i := 0; i < searchResult.ResultCount; i++ {
+			// 获取第i个结果
+			id, _ := ids.GetAsString(i)
+			content, _ := fields.GetColumn("content").GetAsString(i)
+			score := scores[i]
+
+			results = append(results, SearchResult{
+				ID:      id,
+				Content: content,
+				Score:   score,
+			})
+		}
+	}
+
+	return results, nil
 }
